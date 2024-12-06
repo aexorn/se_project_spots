aa4655d46b281988681606c0d5e4073cea0173e6
ashten <asheorion@gmail.com>
2024-12-06 13:05:33 -0800

    filled objects with data from cards and console.log'd the result

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,28 @@
-const initialCards=[
-  { name:
-    link:
-  }
-  { name:
-    link:
-  }
-  { name:
-    link:
-  }
-  { name:
-    link:
-  }
-  { name:
-    link:
-  }
-  { name:
-    link:
-  }
-]
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "./images/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant Terrace",
+    link: "./images/2-photo-by-ceiline-from-pexels-min.jpg",
+  },
+  {
+    name: "An Outdoor Cafe",
+    link: "./images/3-photo-by-tubanur-dogan-from-pexels-min.jpg",
+  },
+  {
+    name: "A very long bridge over the forest canopy",
+    link: "./images/4-photo-by-maurice-laschet-from-pexels-min.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "./images/5-photo-by-van-anh-nguyen-from-pexels-min.jpg",
+  },
+  {
+    name: "Mountain House",
+    link: "./images/6-photo-by-moritz-feldmann-from-pexels-min.jpg",
+  },
+];
+
+console.log(initialCards);
