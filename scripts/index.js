2e71dca85fb68e4d4a8af509fc682edec0475053
ashten <aexorn@hotmail.com>
2025-01-12 15:32:22 -0800

    submission revisions: cursor:pointer on interactive elements, alt tags and titles, font adjustments

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,32 @@ const initialCards = [
   {
     name: "Val Thorens",
     link: "./images/1-photo-by-moritz-feldmann-from-pexels.jpg",
+    alt:"a monitor in darkened room displays an image of a snow covered cabin in the mountains"
   },
   {
     name: "Restaurant Terrace",
     link: "./images/2-photo-by-ceiline-from-pexels-min.jpg",
+    alt: "flowers grow around the edges of the dark green awning of a restaurant on a cobbled street corner while the patio seating below sits empty on an overcast day"
   },
   {
     name: "An Outdoor Cafe",
     link: "./images/3-photo-by-tubanur-dogan-from-pexels-min.jpg",
+    alt: "from above we see people gathered around folding black tables in a bustling intimate outdoor patio space"
   },
   {
     name: "A very long bridge over the forest canopy",
     link: "./images/4-photo-by-maurice-laschet-from-pexels-min.jpg",
+    alt: "a bridge narrows as it sprawls into the mass of treetops that dominate the rest of the frame with green foliage just beginning to twinge orange with the season"
   },
   {
     name: "Tunnel with morning light",
     link: "./images/5-photo-by-van-anh-nguyen-from-pexels-min.jpg",
+    alt: "extending into the frame for what feels like a great distance, an impressively high wood ceilinged tunnel lets cool morning light flood in the windows that make up both its sides. "
   },
   {
     name: "Mountain House",
     link: "./images/6-photo-by-moritz-feldmann-from-pexels-min.jpg",
+    alt: "a snow storm whips around an isolate rustic and weather worn cabin which is in stark contrast to the tall, powder caked aspens in the background"
   },
 ];
 
@@ -43,13 +49,10 @@ const cardList = document.querySelector(".cards__list");
 function getCardElement(data) {
   console.log(data);
   const card = cardTemplate.content.querySelector(".card").cloneNode(true);
-  const cardName = card.querySelector(".card__title");
-  cardName.textContent = data.name;
+  card.querySelector(".card__title").textContent = data.name;
   card.querySelector(".card__image").src = data.link;
-  data.alt = cardName;
+  card.querySelector(".card__image").alt = data.alt;
   return card;
-  // const cardLink = (card.querySelector(".card__image").src);
-  // cardLink = data.link;
 }
 
 function openModal() {
